7305dc2cf171d6f960aaa90975a7d299739d38a1
jakobpalmborg <jakob.palmborg@gmail.com>
2023-09-01 11:07:06 +0200

    add classes shape, circle and rectange

---
+++ src/main/java/org/example/Circle.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    @Override
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/Rectange.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class Rectange extends Shape {
+
+    private double length;
+    private double height;
+
+    public Rectange(double length, double height) {
+        this.length = length;
+        this.height = height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return length * height;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return length * 2 + height * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectange{" +
+                "length=" + length +
+                ", height=" + height +
+                '}';
+    }
+}
